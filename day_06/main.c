9079418ac2c1ef05c22c4d7614b6f46b1f477ffc
Aubrey Rhodes <a.rhodes@calendly.com>
2019-12-12 16:38:34 -0500

    Day 6 p2

--- day_06/main.c
+++ day_06/main.c
@@ -4,10 +4,18 @@
 
 #include "file.h"
 
+typedef struct Body {
+    char* name;
+    struct Body* firstSat;
+    struct Body* nextSibling;
+    struct Body* parent;
+} Body;
+
 typedef struct Node {
     char* base;
     char* sat;
     struct Node* next;
+    struct Node* prev;
 } Node;
 
 typedef struct Queue {
@@ -15,11 +23,6 @@ typedef struct Queue {
     Node* last;
 } Queue;
 
-typedef struct Body {
-    char* name;
-    struct Body* firstSat;
-    struct Body* nextSibling;
-} Body;
 
 static Body* getBody(Body* root, char* name) {
     if(strcmp(name, "COM") == 0) {
@@ -48,17 +51,21 @@ static void addToQueue(Queue* queue, char* base, char* sat) {
     node->base = base;
     node->sat = sat;
     node->next = NULL;
-    if(queue->last != NULL) queue->last->next = node;
+    node->prev = NULL;
+    if(queue->last != NULL){
+        queue->last->next = node;
+        node->prev = queue->last;
+    }
     if(queue->first == NULL) queue->first = node;
     queue->last = node;
 }
 
 static Node* getFromQueue(Queue* queue) {
     Node* node = queue->first;
+    if(node == NULL) return NULL;
     queue->first = node->next;
     return node;
 }
-
 static void loadQueue(Queue* queue, char* source) {
     int start = 0;
     int end = 0;
@@ -100,6 +107,7 @@ static void addSat(Body* base, char* name) {
     satBody->name = name;
     satBody->firstSat = NULL;
     satBody->nextSibling = NULL;
+    satBody->parent = base;
 
     if(base->firstSat == NULL) {
         base->firstSat = satBody;
@@ -142,8 +150,44 @@ static int countGraph(Body* root, int base) {
     return sum;
 }
 
-int main(int argc, char const *argv[])
-{
+int findTransferDistance(Body* root, char* from, char* to) {
+    Body* fromBody = getBody(root, from);
+    Body* toBody = getBody(root, to);
+    Queue* fromQueue = malloc(sizeof(Queue));
+    Queue* toQueue = malloc(sizeof(Queue));
+
+    while(fromBody->parent != NULL) {
+        addToQueue(fromQueue, fromBody->parent->name, NULL);
+        fromBody = fromBody->parent;
+    }
+
+    while(toBody->parent != NULL) {
+        addToQueue(toQueue, toBody->parent->name, NULL);
+        toBody = toBody->parent;
+    }
+
+    Node* toNode = toQueue->last;
+    Node* fromNode = fromQueue->last;
+    while(strcmp(toNode->base, fromNode->base) == 0) {
+        toNode = toNode->prev;
+        fromNode = fromNode->prev;
+    }
+
+    int distance = 0;
+    while(toNode != NULL) {
+        distance++;
+        toNode = toNode->prev;;
+    }
+
+    while(fromNode != NULL) {
+        distance++;
+        fromNode = fromNode->prev;
+    }
+
+    return distance;
+}
+
+int main(int argc, char const *argv[]) {
     char* source = readFile("input.txt");
     Queue* queue = malloc(sizeof(Queue));
     queue->first = NULL;
@@ -153,10 +197,14 @@ int main(int argc, char const *argv[])
     graph->name = NULL;
     graph->firstSat = NULL;
     graph->nextSibling = NULL;
+    graph->parent = NULL;
     buildGraph(graph, queue);
 
     int solution = countGraph(graph, 0);
     printf("Solution 1: %d\n", solution);
+
+    int solution2 = findTransferDistance(graph, "YOU", "SAN");
+    printf("Solution 2: %d\n", solution2);
     
     return 0;
 }
